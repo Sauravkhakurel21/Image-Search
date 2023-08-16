1c862a1ed5a1e831cbee94cb06347f6647195c91
Sauravkhaurel21 <khakurelsaurav21@gmail.com>
2023-08-16 16:29:32 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import UseAxios from "./Hooks/UseAxios";
+import Images from "./component/Images";
+import Jumbotron from "./component/Jumbotron";
+import Searchfield from "./component/Searchfield";
+
+export const Imagecontext = createContext();
 
 function App() {
+  const [searchimage, setsearchimage] = useState("");
+  const { response, isLoading, error, fetchdata } = UseAxios(
+    `search/collections?page=1&query=office&client_id=PBOYhj_qmT4NMfdWuIkIaiPE5VKsbbbg1_SeWcdIK3U`
+  );
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchdata,
+    searchimage,
+    setsearchimage,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Imagecontext.Provider value={value}>
+        <Jumbotron>
+          <Searchfield />
+        </Jumbotron>
+
+        <Images />
+      </Imagecontext.Provider>
+    </>
   );
 }
 
